3e7aa535a7499bb998cae83abc80c2dc419069d1
Alexei <alexeim763@gmail.com>
2019-06-07 10:26:29 -0500

    Add first tests

--- spec/tttelematics_api_spec.rb
+++ spec/tttelematics_api_spec.rb
@@ -1,5 +1,16 @@
 RSpec.describe TTTelematics::API, "TTTelematics" do
-  describe "config client" do
+  describe "config client incomplete and no attributes"  do
+    it 'should raise an error'  do
+      #
+    end
+  end
 
+  describe 'config client with params' do
+    # should return a client config
+    it 'should return a Client Object' do
+    end
+
+    it 'should a return client with the correct params' do
+    end
   end
 end
