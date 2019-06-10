c7ba91f070a51c3c6196a30bcea0959df62c0f50
Alexei <alexeim763@gmail.com>
2019-06-10 17:08:01 -0500

    Added new class for client TTTelematics

---
+++ lib/TTTelematics/client.rb
@@ -0,0 +1,24 @@
+module TTTelematics
+  module API
+    class Client
+      attr_reader :api_key, :username, :account, :password
+      def initialize(api_key, username, account, password)
+        raise TypeError unless validate_options?(api_key, username, account, password)
+        @api_key = api_key
+        @username = username
+        @account = account 
+        @password = password 
+      end
+
+      private
+      def validate_options?(*args)
+        flag = true
+        args.each do |a|
+          flag = (!a.nil? && a.class == String && !a.empty?)
+          break unless flag
+        end
+        return flag
+      end
+    end
+  end
+end
