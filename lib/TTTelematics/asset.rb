41fab596dc04a7f9777b30b02fe65730832b8c26
Alexei <alexeim763@gmail.com>
2019-06-10 16:42:23 -0500

    Remove validator1

--- lib/TTTelematics/asset.rb
+++ lib/TTTelematics/asset.rb
@@ -1,6 +1,21 @@
 module TTTelematics
   module API
     class Asset
-    end
+      PERMIT_KEYS =  { filterstring: 'string', 
+                     objectgroupname: 'string', 
+                     oungroupedonlye: 'string',
+                     objectno: 'string',
+                     objectuid: 'string',
+                     externalid: 'string'}
+
+      def initialize(client)
+        raise TypeError if !(client.is_a?(Client)) 
+        @client = client
+      end
+
+      def objects(options)
+        validate_keys?(permitted_keys, options.keys.map(&:to_s))
+        validate_values?(PERMIT_KEYS, options)
+      end
   end
 end
