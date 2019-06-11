ebe8e654a8bb9d491092295d8d412cdd1be1ee92
Alexei <alexeim763@gmail.com>
2019-06-11 13:03:10 -0500

    Update spec for new param

--- spec/TTTelematics/asset_spec.rb
+++ spec/TTTelematics/asset_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe TTTelematics::API::Asset, "TTTelematics Assets" do
     end
 
     it 'should return a array objects if arguments it is OK' do
-      stub_request(:get, "https://csv.telematics.tomtom.com/extern?account=username&action=showObjectReportExtern&api_key=apikey&lang=en&objectno=JETTA&outputformat=json&password=password&username=client").
+      stub_request(:get, "https://csv.telematics.tomtom.com/extern?account=username&action=showObjectReportExtern&apikey=apikey&lang=en&objectno=JETTA&outputformat=json&password=password&username=client").
         to_return(status: 200, body: [{'objectno': 'JETTA'}].to_json , headers: {})
         response = asset.objects({objectno: 'JETTA'})
         expect(JSON.parse(response).first['objectno']).to eq('JETTA')
