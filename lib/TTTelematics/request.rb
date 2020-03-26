2e84320f13f3019e8c4f3234c7760e290afdcd7b
Alexei Mamani <alexeim763@gmail.com>
2020-03-25 22:32:38 -0500

    Remove require bundler

--- lib/TTTelematics/request.rb
+++ lib/TTTelematics/request.rb
@@ -3,15 +3,14 @@ module TTTelematics
     class Request < Base
       require 'faraday'
       require 'json'
-      require 'byebug'
       LANG = 'en'.freeze
       def get(action, params, format = 'json')
-        if (action.is_a?(String)) && 
+        if (action.is_a?(String)) &&
            !(action.nil?) &&
            !(action.empty?)
-           
+
           params = generate_params(action, params, format)
-          response = Faraday.get(BASE_URL, params) 
+          response = Faraday.get(BASE_URL, params)
           handle_response(response)
           response
         else
@@ -27,7 +26,7 @@ module TTTelematics
            params[:apikey] = @client.api_key
            params[:username] = @client.username
            params[:password] = @client.password
-           params[:lang] = LANG 
+           params[:lang] = LANG
            params
         end
 
